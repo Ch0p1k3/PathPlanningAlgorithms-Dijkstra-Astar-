a9fdcc4bc24d1001db71f9281e0eb72373becafb
Chop <ch00p.228@gmail.com>
2020-11-17 18:33:25 +0300

    The unexplored Dijkstra was added

--- Src/search.cpp
+++ Src/search.cpp
@@ -13,7 +13,7 @@ Search::~Search() {}
 std::optional<Node> Search::GetNeighbours(Node& v, int i, int j, const Map &map, const EnvironmentOptions &options)
 {
     if (map.CellOnGridAndIsTraversable(v.i + i, v.j + j)) {
-        if (abs(i - j) == 0) {
+        if (abs(i) - abs(j) == 0) {
             if (options.allowdiagonal) {
                 bool f1 = map.CellOnGridAndIsTraversable(v.i, v.j + j);
                 bool f2 = map.CellOnGridAndIsTraversable(v.i + i, v.j);
@@ -48,16 +48,19 @@ SearchResult Search::startSearch(ILogger *Logger, const Map &map, const Environm
     open.push_back(Node(start.i, start.j, 0, 0, nullptr));
     while (!open.empty()) {
         ++cntSt;
-        auto v = std::min_element(open.begin(), open.end());
-        close[v->i][v->j] = *v;
+        auto l = std::min_element(open.begin(), open.end());
+        close[l->i][l->j] = *l;
+        int inI = l->i, inJ = l->j;
         ++cntClose;
-        open.erase(v);
+        open.erase(l);
+        auto v = &close[inI][inJ];
         if (v->i == goal.i && v->j == goal.j) {
             searchedGoal = &close[v->i][v->j];
             break;
         }
         for (int i = -1; i <= 1; ++i) {
             for (int j = -1; j <= 1; ++j) {
+                if (i == 0 && j == 0) continue;
                 if (auto neighbour = GetNeighbours(close[v->i][v->j], i, j, map, options)) {
                     if (close[neighbour->i][neighbour->j].i == -1) {
                         auto it = std::find_if(open.begin(), open.end(), [neighbour](Node el) {
@@ -74,31 +77,31 @@ SearchResult Search::startSearch(ILogger *Logger, const Map &map, const Environm
         }
     }
 
-    sresult.pathfound = searchedGoal == nullptr;
+    sresult.pathfound = (searchedGoal != nullptr);
     sresult.pathlength = sresult.pathfound ? searchedGoal->g : 0;
     sresult.nodescreated =  open.size() + cntClose;
     sresult.numberofsteps = cntSt;
-
-    while (searchedGoal) {
-        lppath.push_front(*searchedGoal);
-        searchedGoal = searchedGoal->parent;
-    }
-    
-    if (!lppath.empty()) {
-        hppath.push_front(lppath.front());
-        if (lppath.size() != 1) {
-            auto cur = std::next(lppath.begin());
-            auto prev = lppath.begin();
-            for (auto it = next(lppath.begin(), 2); it != lppath.end(); next(it)) {
-                if (!(it->i - cur->i == cur->i - prev->i && it->j - cur->j == cur->j - prev->j)) {
-                    hppath.push_back(*cur);
+    if (sresult.pathfound) {
+        while (searchedGoal) {
+            lppath.push_front(*searchedGoal);
+            searchedGoal = searchedGoal->parent;
+        }
+        
+        if (!lppath.empty()) {
+            hppath.push_front(lppath.front());
+            if (lppath.size() != 1) {
+                auto cur = std::next(lppath.begin());
+                auto prev = lppath.begin();
+                for (auto it = next(lppath.begin(), 2); it != lppath.end(); it = next(it)) {
+                    if (!(it->i - cur->i == cur->i - prev->i && it->j - cur->j == cur->j - prev->j)) {
+                        hppath.push_back(*cur);
+                    }
+                    prev = cur;
+                    cur = it;
                 }
-                prev = cur;
-                cur = it;
             }
         }
     }
-
     sresult.time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - time).count();
     sresult.hppath = &hppath;
     sresult.lppath = &lppath;
