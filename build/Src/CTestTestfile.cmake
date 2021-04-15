63a36db71ed0ea3c4e67e79106e8491749af0d92
Chop <ch00p.228@gmail.com>
2021-04-15 15:23:22 +0300

    .

--- build/Src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Projects/PathPlanningAlgorithms-Dijkstra-Astar-/Src
-# Build directory: D:/Projects/PathPlanningAlgorithms-Dijkstra-Astar-/build/Src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
